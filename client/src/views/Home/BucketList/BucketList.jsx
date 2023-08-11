6dec2c7851deb51feaca7d4ae6bce43dc3d4c6c3
Anh Dao <addao823@gmail.com>
2023-08-11 10:34:22 -0700

    Clearing import errors

--- client/src/views/Home/BucketList/BucketList.jsx
+++ client/src/views/Home/BucketList/BucketList.jsx
@@ -1,40 +1,40 @@
 import React, { useEffect, useState } from "react";
 import Bucket from "../Bucket/Bucket";
 import {
-  useHomeDispatch,
-  useHomeState,
-} from "../../../context/HomePage/Home.Context";
-import { loadUserBucket } from "../../../context/HomePage/Home.action";
+	useHomeDispatch,
+	useHomeState,
+} from "../../../state/HomePage/Home.context";
+import { loadUserBucket } from "../../../state/HomePage/Home.action";
 
 const BucketView = () => {
-  const state = useHomeState();
-  const dispatch = useHomeDispatch();
+	const state = useHomeState();
+	const dispatch = useHomeDispatch();
 
-  useEffect(() => {
-    dispatch(loadUserBucket("123"));
-  }, []);
+	useEffect(() => {
+		dispatch(loadUserBucket("123"));
+	}, []);
 
-  return (
-    <div className={"flex flex-col w-72 bg-white shadow-md "}>
-      <div className="flex justify-between px-5">
-        <p>Income</p>
-        <p>{state.income}</p>
-      </div>
-      {state.bucketList.map((bucket, index) => (
-        <Bucket
-          key={index}
-          name={bucket.name}
-          spend={bucket.spend}
-          limit={bucket.limit}
-          data-testid={`bucket-${index}`}
-        />
-      ))}
-      <div className="flex justify-between px-5">
-        <p>Leftover</p>
-        <p>{state.leftover}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={"flex flex-col w-72 bg-white shadow-md "}>
+			<div className="flex justify-between px-5">
+				<p>Income</p>
+				<p>{state.income}</p>
+			</div>
+			{state.bucketList.map((bucket, index) => (
+				<Bucket
+					key={index}
+					name={bucket.name}
+					spend={bucket.spend}
+					limit={bucket.limit}
+					data-testid={`bucket-${index}`}
+				/>
+			))}
+			<div className="flex justify-between px-5">
+				<p>Leftover</p>
+				<p>{state.leftover}</p>
+			</div>
+		</div>
+	);
 };
 
 export default BucketView;
--- client/src/views/Home/HomePage/HomePage.jsx
+++ client/src/views/Home/HomePage/HomePage.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import BucketList from "../BucketList/BucketList";
 import SpendingTable from "../SpendingTable/SpendingTable";
-import HomeProvider from "../../../context/HomePage/Home.provider";
+import HomeProvider from "../../../state/HomePage/Home.provider";
 import Appbar from "../Appbar/Appbar";
 
 const HomePage = (data) => {
-  return (
-    <HomeProvider>
-      <Appbar />
-      <div className="container mx-auto flex flex-col">
-        <div className="w-full flex justify-between space-x-5">
-          <BucketList />
-          <div className={"w-4/5"}>
-            <span>Welcome</span>
-            <div className="w-full flex justify-center">
-              <button>{"<"}</button>
-              <div>currentMonth</div>
-              <button>{">"}</button>
-            </div>
-            <SpendingTable />
-          </div>
-        </div>
-      </div>
-    </HomeProvider>
-  );
+	return (
+		<HomeProvider>
+			<Appbar />
+			<div className="container mx-auto flex flex-col">
+				<div className="w-full flex justify-between space-x-5">
+					<BucketList />
+					<div className={"w-4/5"}>
+						<span>Welcome</span>
+						<div className="w-full flex justify-center">
+							<button>{"<"}</button>
+							<div>currentMonth</div>
+							<button>{">"}</button>
+						</div>
+						<SpendingTable />
+					</div>
+				</div>
+			</div>
+		</HomeProvider>
+	);
 };
 
 export default HomePage;
