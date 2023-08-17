81b7b15da53cfb8f17e713be70bbfe642848b3b3
Anh Dao <addao823@gmail.com>
2023-08-17 14:08:12 -0700

    Draft new route to get transaction by months

--- server/src/routes/transaction.ts
+++ server/src/routes/transaction.ts
@@ -1,6 +1,5 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, {Request} from "express";
 import {ParamsDictionary } from "express-serve-static-core";
-import {assertHasUser, RequestWithUser } from "../middleware/authMiddleware";
 import Transaction,{iTransaction} from "../models/transaction";
 import Bucket from "../models/bucket";
 
@@ -9,15 +8,17 @@ const router = express.Router()
 type TypedBodyReq<T> = Request<ParamsDictionary, {}, T>
 
 const getUserId = (req: Request): string => {
-  const verifiedReq = assertHasUser(req);
-  return verifiedReq.user.id;
+  if (!req.user) {
+    throw Error("Can't find user in request")
+  }
+  return req.user.id;
 } 
 
 /**
  * @route POST /transaction
  * for insert on transaction
  * expect req.user
- * returna transaction document in JSON format
+ * return a transaction document in JSON format
  */
 type iTransactionInput = Omit<iTransaction, "user">
 router.post("/", async (req: TypedBodyReq<iTransactionInput>, res, next) => {
@@ -58,6 +59,26 @@ router.post("/multi", async (req: TypedBodyReq<iMultTransactionInput>, res, next
 });
 
 /**
+ * @route GET /transaction/:month
+ * for getting detail about all transaction within a month
+ * 
+ * return multiple transaction within a month
+ */
+router.get("/:month", async (req, res, next) => {
+  try {
+    const userId = getUserId(req);
+    const transactionDocs = await Transaction.aggregate([{ $addFields: { $month: '$date' } }, { $match: { month: req.params.month, user: userId } }, { $project: { month: 0 } }])
+    if (!transactionDocs) throw new Error("Something is wrong the document");
+    const transactionsJSON = transactionDocs.map(doc => {
+      doc.toJSON();
+    })
+    res.status(200).json(transactionsJSON);
+  } catch (error) {
+    next(error)
+  }
+})
+
+/**
  * @route GET /transaction/:id
  * for getting detail about a transaction
  * expect req.user
@@ -80,7 +101,7 @@ router.get("/:id", async (req,res,next) => {
  * @route GET /transaction/bucket/:name
  * for gettting the bucket 
  */
-router.get("/transaction/bucket/:name", async (req,res,next) => {
+router.get("/bucket/:name", async (req,res,next) => {
   try {
     const userId = getUserId(req);
     const bucketDoc = await Bucket.findOne({name: req.params.name, user: userId});
