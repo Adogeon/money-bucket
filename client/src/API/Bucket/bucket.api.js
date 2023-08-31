cb186649f6a8d5a987b3fc56e66ab37b3392a3ea
Anh Dao <addao823@gmail.com>
2023-08-30 19:56:37 -0700

    Draft getBucketDetail api func

--- client/src/API/Bucket/bucket.api.js
+++ client/src/API/Bucket/bucket.api.js
@@ -1,13 +1,23 @@
 export const getUserBucket = async () => {
-  const result = await [
-    { id: 1, name: "Essential", spend: 60, limit: 100 },
-    { id: 2, name: "Food", spend: 20, limit: 200 },
-    { id: 3, name: "Media", spend: 40, limit: 50 },
-  ];
-  return result;
+	const result = await [
+		{ id: 1, name: "Essential", spend: 60, limit: 100 },
+		{ id: 2, name: "Food", spend: 20, limit: 200 },
+		{ id: 3, name: "Media", spend: 40, limit: 50 },
+	];
+	return result;
 };
 
-export const getBucketDetail = async (bucketId) => {
-  const result = await {};
-  return result;
+export const getBucketDetail = async (bucketName, userToken) => {
+	try {
+		const fetchResponse = await fetch(`/bucket/${bucketName}`, {
+			headers: { Authorization: `Bearer ${userToken}` },
+		});
+		if (fetchResponse.status !== 200) {
+			throw new Error(fetchResponse.statusText);
+		}
+		const result = await fetchResponse.json();
+		return result;
+	} catch (error) {
+		console.log(error);
+	}
 };
