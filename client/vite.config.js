1d22a6e6c8d5452db2b3f9bd1800ba2d707bcc75
Anh Dao <addao823@gmail.com>
2023-08-31 10:14:45 -0700

    debug vite proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,35 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  proxy: {
-    "/api": {
-      target: "https://localhost:5000",
-      changeOrign: true,
-      secure: false,
-      ws: false,
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000/",
+				changeOrign: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+				secure: false,
+				configure: (proxy, _options) => {
+					console.log(proxy);
+					proxy.on("error", (err, _req, _res) => {
+						console.log("proxy error", err);
+					});
+					proxy.on("proxyReq", (proxyReq, req, _res) => {
+						console.log(
+							"Sending Request to the Target:",
+							req.method,
+							req.url
+						);
+					});
+					proxy.on("proxyRes", (proxyRes, req, _res) => {
+						console.log(
+							"Received Response from the Target:",
+							proxyRes.statusCode,
+							req.url
+						);
+					});
+				},
+			},
+		},
+	},
 });
