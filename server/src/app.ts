3705ced26c3cc4813871448b12824e5cd2367609
Anh Dao <addao823@gmail.com>
2023-08-31 10:14:25 -0700

    add dotenv and body-parser to server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
 //import { jwtMiddleware } from 'middleware/jwtMiddleware';
 import routers from "./routes";
 
+
 const app: Express = express();
 
+//middleware
+app.use(bodyParser.json());
 //app.use("/", jwtMiddleware())
 
 app.use(routers);
