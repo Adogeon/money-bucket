061aa556b741b52c1e1b0487c11b304ffc7d8f47
anhdao <addao823@gmail.com>
2023-09-21 16:26:47 -0700

    Adding eslint rules

--- client/src/views/Home/SpendingTable/SpendingTable.jsx
+++ client/src/views/Home/SpendingTable/SpendingTable.jsx
@@ -1,69 +1,70 @@
-import React, { useEffect, useState } from "react";
-import SpendingRow from "./SpendingRow";
-import { fetchUserLoginToken } from "../../../API/auth/auth.api";
-import { getMonthTransactions } from "../../../API/transaction/transaction.api";
+import React, {useEffect, useState} from 'react';
+import SpendingRow from './SpendingRow';
+import {fetchUserLoginToken} from '../../../API/auth/auth.api';
+import {getMonthTransactions} from '../../../API/transaction/transaction.api';
 import {
-	useHomeDispatch,
-	useHomeState,
-} from "../../../state/HomePage/Home.context";
-import { useAuthToken } from "../../../state/Auth/auth.context";
+  useHomeDispatch,
+  useHomeState,
+} from '../../../state/HomePage/Home.context';
+import {useAuthToken} from '../../../state/Auth/auth.context';
 
-const SpendingTable = ({ userToken }) => {
-	const homeStore = useHomeState();
-	const homeDispatch = useHomeDispatch();
-	console.log(userToken);
-	// TODO: add action to fetch month transaction
 
-	const [data, setData] = useState([]);
-	const [month, setMonth] = useState("022023");
+const SpendingTable = ({userToken}) => {
+  const homeStore = useHomeState();
+  const homeDispatch = useHomeDispatch();
+  console.log(userToken);
+  // TODO: add action to fetch month transaction
 
-	const loadData = async () => {
-		//const userToken = await fetchUserLoginToken("thomas", "12345");
+  const [data, setData] = useState([]);
+  const [month, setMonth] = useState('022023');
 
-		const transactions = await getMonthTransactions(userToken, month);
-		if (transactions === undefined) {
-			transactions = [];
-		}
-		setData(transactions);
-	};
+  const loadData = async () => {
+    // const userToken = await fetchUserLoginToken("thomas", "12345");
 
-	useEffect(() => {
-		loadData();
-	}, [month, userToken]);
+    const transactions = await getMonthTransactions(userToken, month);
+    if (transactions === undefined) {
+      transactions = [];
+    }
+    setData(transactions);
+  };
 
-	return (
-		<table className="w-full text-center shadow-md">
-			<thead className="border-b bg-gray-800">
-				<tr>
-					<th
-						scope="col"
-						className="text-sm font-medium text-white px-6 py-4">
+  useEffect(() => {
+    loadData();
+  }, [month, userToken]);
+
+  return (
+    <table className="w-full text-center shadow-md">
+      <thead className="border-b bg-gray-800">
+        <tr>
+          <th
+            scope="col"
+            className="text-sm font-medium text-white px-6 py-4">
 						Date
-					</th>
-					<th
-						scope="col"
-						className="text-sm font-medium text-white px-6 py-4">
+          </th>
+          <th
+            scope="col"
+            className="text-sm font-medium text-white px-6 py-4">
 						Summary
-					</th>
-					<th
-						scope="col"
-						className="text-sm font-medium text-white px-6 py-4">
+          </th>
+          <th
+            scope="col"
+            className="text-sm font-medium text-white px-6 py-4">
 						Bucket
-					</th>
-					<th
-						scope="col"
-						className="text-sm font-medium text-white px-6 py-4">
+          </th>
+          <th
+            scope="col"
+            className="text-sm font-medium text-white px-6 py-4">
 						Amount
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				{data.map((transaction, index) => (
-					<SpendingRow transaction={transaction} rIndex={index} />
-				))}
-			</tbody>
-		</table>
-	);
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((transaction, index) => (
+          <SpendingRow transaction={transaction} rIndex={index} key={`spendRow-${index}`}/>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default SpendingTable;
