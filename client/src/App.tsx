f5a48c00b3d2d747e30850430ab4fada44b7859e
anhdao <addao823@gmail.com>
2023-10-09 23:25:06 -0700

    Rewrite app router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,9 @@ import {
 	Outlet,
 	Route,
 } from "react-router-dom";
-import { HomePage, AddPage, FillPage } from "./views";
+
 import AuthProvider from "./state/Auth/auth.provider";
 import Appbar from "./components/Appbar/Appbar";
-import TestPage from "./views/test";
 
 const Layout = (): JSX.Element => {
 	return (
@@ -27,13 +26,29 @@ const Layout = (): JSX.Element => {
 	);
 };
 
+const Home = ():JSX.Element => <div>Homepage with summary of current month</div>
+
+const Buckets = ():JSX.Element => <div> List of bucket summary</div>
+const Bucket = ():JSX.Element => <div>Bucket Detail with transactions within the list</div>
+const AddBucket = ():JSX.Element => <div>New page to add a new bucket</div>
+
+const Transactions = ():JSX.Element => <div> List of Transactions with Month selector(Search ?)</div>
+const Transaction = ():JSX.Element => <div> Detail of a Transaction</div>
+const AddTransaction = ():JSX.Element => <div> Page to add a new transaction</div>
+
+
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route element={<Layout />}>
-			<Route path="/" element={<HomePage />} />
-			<Route path="add" element={<AddPage />} />
-			<Route path="fill" element={<FillPage />} />
-			<Route path="test" element={<TestPage />} />
+			<Route path="bucket" element={<Buckets />}>
+				<Route path=":bucketId" element={<Bucket />} />
+				<Route path="new" element={<AddBucket />} />
+			</Route>
+			<Route path="transaction" element={<Transactions />}>
+				<Route path=":transactionId" element={<Transaction />} />
+				<Route path="new"element={<AddTransaction />} />
+			</Route>
+			<Route index element={<Home/>} />
 		</Route>
 	)
 );
