1567333438a4e6ea60c9d8906b7ba4b30d974fa0
anhdao <addao823@gmail.com>
2023-10-21 15:24:41 -0700

    Change seed to use mongoClient

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,7 +1,79 @@
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 const fs = require("fs/promises");
+const bcrypt = require("bcrypt");
 
-const parseCSVToObject = (rawData) => {
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+
+main()
+  .then(console.log("SEEDING COMPLETE!"))
+  .catch(console.error)
+  .finally(() => client.close());
+
+async function main() {
+  await client.connect();
+  console.log("Database connect");
+  const db = client.db("moneybucket");
+
+  const bucketRaw = await fs.readFile("./db/Budget.csv", {
+    encoding: "utf8",
+  });
+  const seedBucketRaw = parseCSVToObject(bucketRaw);
+
+  const transactionRaw = await fs.readFile("./db/personal_transactions.csv", {
+    encoding: "utf8",
+  });
+  const seedTransactionRaw = parseCSVToObject(transactionRaw);
+
+  await db.collection("user").drop();
+  await db.collection("transaction").drop();
+  await db.collection("bucket").drop();
+
+  const hashPassword = await bcrypt.hash("123456", 10);
+
+  const user1 = await db.collection("user").insertOne({
+    username: "thomas",
+    password: hashPassword,
+    currency: "USD",
+  });
+
+  const seedBuckets = seedBucketRaw.map((bucket) => {
+    return {
+      name: bucket["Category"],
+      defaultLimit: { amount: parseFloat(bucket["Budget"]), currency: "USD" },
+      type: "bucket",
+      user: user1.insertedId,
+    };
+  });
+
+  const seedBucketResult = await db
+    .collection("bucket")
+    .insertMany(seedBuckets);
+  const bucketIdsObjs = seedBuckets.map((buckets, index) => ({
+    name: buckets.name,
+    id: seedBucketResult.insertedIds[index],
+  }));
+
+  const bucketIdMap = {};
+  bucketIdsObjs.forEach((object) => {
+    Object.assign(bucketIdMap, { [object.name]: object.id });
+  });
+
+  const seedTransactions = seedTransactionRaw.map((transaction) => {
+    return {
+      summary: transaction["Description"],
+      amount: parseFloat(transaction["Amount"]),
+      currency: "USD",
+      type: transaction["Transaction Type"],
+      date: new Date(transaction["Date"]),
+      bucket: bucketIdMap[transaction["Category"]],
+      user: user1.insertedId,
+    };
+  });
+  await db.collection("transaction").insertMany(seedTransactions);
+}
+
+function parseCSVToObject(rawData) {
   const rows = rawData.split("\r\n");
   const labels = rows[0].split(",");
   const data = rows.slice(1).map((row) => {
@@ -13,83 +85,4 @@ const parseCSVToObject = (rawData) => {
     return result;
   });
   return data;
-};
-
-const seedDB = async () => {
-  try {
-    const bucketRaw = await fs.readFile("./db/Budget.csv", {
-      encoding: "utf8",
-    });
-    const seedBucketRaw = parseCSVToObject(bucketRaw);
-
-    const transactionRaw = await fs.readFile("./db/personal_transactions.csv", {
-      encoding: "utf8",
-    });
-    const seedTransactionRaw = parseCSVToObject(transactionRaw);
-
-    await mongoose.connection.collections["user"].drop();
-    await mongoose.connection.collections["transaction"].drop();
-    await mongoose.connection.collections["bucket"].drop();
-
-    const user1 = await mongoose.connection.collections["user"].insertOne({
-      username: "thomas",
-      password: "123456",
-      currency: "USD",
-    });
-
-    const seedBuckets = seedBucketRaw.map((bucket) => {
-      return {
-        name: bucket["Category"],
-        defaultLimit: { amount: bucket["Budget"], currency: "USD" },
-        type: "bucket",
-        user: user1.insertedId,
-      };
-    });
-
-    const seedBucketResult = await mongoose.connection.collections[
-      "bucket"
-    ].insertMany(seedBuckets);
-    const bucketIdsObjs = seedBuckets.map((buckets, index) => ({
-      name: buckets.name,
-      id: seedBucketResult.insertedIds[index],
-    }));
-
-    const bucketIdMap = {};
-    bucketIdsObjs.forEach((object) => {
-      Object.assign(bucketIdMap, { [object.name]: object.id });
-    });
-
-    const seedTransactions = seedTransactionRaw.map((transaction) => {
-      return {
-        summary: transaction["Description"],
-        amount: transaction["Amount"],
-        currency: "USD",
-        type: transaction["Transaction Type"],
-        date: new Date(transaction["Date"]),
-        bucket: bucketIdMap[transaction["Category"]],
-        user: user1.insertedId,
-      };
-    });
-    await mongoose.connection.collections["transaction"].insertMany(
-      seedTransactions
-    );
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/moneybucket", {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("MONGO CONNECTION OPEN !!!");
-    return seedDB();
-  })
-  .then(() => {
-    mongoose.connection.close();
-    console.log("SEEDING FINISHED");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+}
