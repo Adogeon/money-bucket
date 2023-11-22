5fa4da97dd0e62f59df5fa941798180ca0a2a93a
anhdao <addao823@gmail.com>
2023-11-21 21:40:36 -0800

    Adding styles to month picker

--- client/src/components/Table/MonthPicker.tsx
+++ client/src/components/Table/MonthPicker.tsx
@@ -22,10 +22,48 @@ function MonthPicker({ month, onMonthChange }: MonthPickerProps): JSX.Element {
   };
 
   return (
-    <div className="w-full flex justify-center">
-      <button onClick={goBackAMonth}>{"<"}</button>
-      <div>{displayMonth}</div>
-      <button onClick={goForwardAMonth}>{">"}</button>
+    <div className="w-full flex justify-evenly mb-3">
+      <button
+        className="rounded-full p-2 hover:bg-secondary hover:bg-opacity-10 hover:brightness-125"
+        onClick={goBackAMonth}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-primary"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </button>
+      <div className="font-semibold text-lg text-primary py-2">
+        {displayMonth}
+      </div>
+      <button
+        className="rounded-full p-2 hover:bg-secondary hover:bg-opacity-10 hover:brightness-125"
+        onClick={goForwardAMonth}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-primary"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
