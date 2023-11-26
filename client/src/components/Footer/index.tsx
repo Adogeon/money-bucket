9bebacfef556e781504b85127b500d6c0b16aac3
anhdao <addao823@gmail.com>
2023-11-25 18:03:58 -0800

    Update footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -2,12 +2,6 @@ const Footer = () => {
   return (
     <footer className="bg-secondary">
       <div>@me 2023</div>
-      <a
-        href="https://www.flaticon.com/free-icons/bucket-list"
-        title="bucket list icons"
-      >
-        Bucket list icons created by Freepik - Flaticon
-      </a>
     </footer>
   );
 };
