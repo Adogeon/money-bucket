15ca154f8ef73be501ef0540929a69e0d921536e
Anh Dao <addao823@gmail.com>
2024-06-10 05:24:21 -0700

    remove unused try catch

--- server/src/data/transactionRepo.ts
+++ server/src/data/transactionRepo.ts
@@ -1,67 +1,43 @@
 import models from "../models";
-import { mongoMonthQueryDO } from "./utils/monthQueryDO";
+import { MongoMonthQueryDO } from "./utils/monthQueryDO";
+import type { monthDO, iTransaction } from "src/common/types";
 
 const TransactionDB = models.Transaction;
 
 export default Object.freeze({
-    listByMonth: async function (userId: string, monthDO: monthDO) {
-        try {
-            const filter = {
-                user: userId, date: new mongoMonthQueryDO(monthDO).generateQuery()
-            }
-            const result = await TransactionDB.find(filter).sort({ date: -1 }).lean()
-            return result;
-        } catch (error) {
-            throw error;
+    listByMonth: async function (userId: string, month: monthDO) {
+        const filter = {
+            user: userId, date: new MongoMonthQueryDO(month).generateQuery()
         }
+        const result = await TransactionDB.find(filter).sort({ date: -1 }).lean()
+        return result;
     },
-    listByMonthAndBucket: async function (userId: string, monthDO: monthDO, bucketId: string) {
-        try {
-            const transactionList = await TransactionDB.find({
-                user: userId,
-                date: new mongoMonthQueryDO(monthDO).generateQuery(),
-                $or: [
-                    { from: bucketId },
-                    { to: bucketId }
-                ]
-            }).sort({ date: -1 }).lean({ getters: true });
-
-            return transactionList;
-        } catch (error) {
-            throw error;
-        }
+    listByMonthAndBucket: async function (userId: string, month: monthDO, bucketId: string) {
+        const transactionList = await TransactionDB.find({
+            user: userId,
+            date: new MongoMonthQueryDO(month).generateQuery(),
+            $or: [
+                { from: bucketId },
+                { to: bucketId }
+            ]
+        }).sort({ date: -1 }).lean({ getters: true });
+        return transactionList;
     },
     searchTransactionById: async function (transactionId: string) {
-        try {
-            const result = await TransactionDB.findOne({ _id: transactionId }).lean().populate({ path: "from", match: "_id name" }).populate({ path: "to", match: "_id name" });
-            return result;
-        } catch (error) {
-            throw error;
-        }
+        const result = await TransactionDB.findOne({ _id: transactionId }).lean().populate({ path: "from", match: "_id name" }).populate({ path: "to", match: "_id name" });
+        return result;
     },
     addNewTransaction: async function (transactionInput: Partial<iTransaction>) {
-        try {
-            const newTransaction = new TransactionDB(transactionInput);
-            newTransaction.save();
-            return newTransaction;
-        } catch (error) {
-            throw error;
-        }
+        const newTransaction = new TransactionDB(transactionInput);
+        await newTransaction.save();
+        return newTransaction;
     },
     updateTransaction: async function (transactionId: string, transactionUpdate: Partial<iTransaction>) {
-        try {
-            const updateTransaction = await TransactionDB.findByIdAndUpdate(transactionId, transactionUpdate, { new: true, lean: true });
-            return updateTransaction;
-        } catch (error) {
-            throw error;
-        }
+        const updateTransaction = await TransactionDB.findByIdAndUpdate(transactionId, transactionUpdate, { new: true, lean: true });
+        return updateTransaction;
     },
     deleteTransaction: async function (transactionId: string) {
-        try {
-            const deleteTransaction = await TransactionDB.findByIdAndDelete(transactionId, { lean: true });
-            return deleteTransaction?._id === transactionId ?? false;
-        } catch (error) {
-            throw error;
-        }
+        const deleteTransaction = await TransactionDB.findByIdAndDelete(transactionId, { lean: true });
+        return deleteTransaction?._id === transactionId ?? false;
     }
 })
