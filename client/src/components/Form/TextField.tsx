6c6c14d543bbd2a71b05fd26c11a10429ef5c4d5
Anh Dao <addao823@gmail.com>
2024-06-23 18:47:29 -0700

    changing test library to vitest

---
+++ client/src/components/Form/TextField.tsx
@@ -0,0 +1,11 @@
+const TextField = () => {
+  return (
+    <section className=".input-section">
+      <label htmlFor="label">Label</label>
+      <input id="label" />
+      <small>error-text</small>
+    </section>
+  );
+};
+
+export default TextField;
