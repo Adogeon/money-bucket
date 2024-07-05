c576d5f5d0ceb58fbcf29903cdcffda33ca8799f
Anh Dao <addao823@gmail.com>
2024-07-04 23:04:35 -0700

    Adding LoginForm to router

--- client/src/API/auth.api.ts
+++ client/src/API/auth.api.ts
@@ -2,12 +2,15 @@ export interface iAuthToken {
 	token: string
 }
 
-export const fetchUserLoginToken = async (username: string, password: string): Promise<iAuthToken> => {
-	const requestBody = { username, password };
+export type LoginRequestDAO = {
+	username: string,
+	password: string
+}
+export const fetchUserLoginToken = async (input: LoginRequestDAO): Promise<iAuthToken> => {
 	const fetchResult = await fetch("/api/auth/login", {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(requestBody),
+		body: JSON.stringify(input),
 	});
 	if (!fetchResult.ok) {
 		throw new Error("Server response problem");
@@ -15,12 +18,16 @@ export const fetchUserLoginToken = async (username: string, password: string): P
 	return await fetchResult.json();
 };
 
-export const fetchUserRegisterToken = async (username: string, password: string, email: string): Promise<iAuthToken> => {
-	const requestBody = { username, password, email };
+export type RegisterRequestDAO = {
+	username: string,
+	password: string,
+	email: string
+}
+export const fetchUserRegisterToken = async (input: RegisterRequestDAO): Promise<iAuthToken> => {
 	const fetchResult = await fetch("/api/auth/register", {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(requestBody),
+		body: JSON.stringify(input),
 	});
 	if (!fetchResult.ok) {
 		throw new Error("Server response problem");
--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -1,74 +1,37 @@
 import { useMutation } from "@tanstack/react-query";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { fetchUserLoginToken, iAuthToken } from "../API/auth.api";
+import {
+  LoginRequestDAO,
+  fetchUserLoginToken,
+  iAuthToken,
+} from "../API/auth.api";
 import { useAuth } from "../context/AuthContext";
-import { useState } from "react";
-import type { FormEvent } from "react";
+import LoginForm from "../components/Form/LoginForm";
 
 export const Route = createFileRoute("/login")({
   component: LoginPage,
 });
 
 function LoginPage() {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-
   const auth = useAuth();
   const navigate = useNavigate({ from: "/login" });
 
-  const loginQuery = useMutation<
-    iAuthToken,
-    unknown,
-    { username: string; password: string },
-    unknown
-  >({
-    mutationFn: ({
-      username,
-      password,
-    }: {
-      username: string;
-      password: string;
-    }) => fetchUserLoginToken(username, password),
-    onError: () => {
-      throw new Error("Login error");
-    },
-    onSuccess: (data) => {
-      auth.setToken(data.token);
-      navigate({ to: "/", replace: true });
-    },
-  });
-
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    loginQuery.mutate({ username, password });
-  };
+  const loginQuery = useMutation<iAuthToken, unknown, LoginRequestDAO, unknown>(
+    {
+      mutationFn: (input: LoginRequestDAO) => fetchUserLoginToken(input),
+      onError: () => {
+        throw new Error("Login error");
+      },
+      onSuccess: (data) => {
+        auth.setToken(data.token);
+        navigate({ to: "/", replace: true });
+      },
+    }
+  );
 
   return (
     <div className="p-2">
-      <div>You must log in!</div>
-      <div className="h-2" />
-      <form onSubmit={onSubmit}>
-        <div className="flex space-x-3">
-          <input
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-            className="border p-1 px-2 rounded"
-          />
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="border p-1 px-2 rounded"
-          />
-          <button
-            type="submit"
-            className="text-sm bg-blue-500 text-white border inline-block py-1 px-2 rounded"
-          >
-            Login
-          </button>
-        </div>
-      </form>
+      <LoginForm submitLogin={(input) => loginQuery.mutate(input)} />;
     </div>
   );
 }
